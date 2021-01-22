84cae06486e0ac4f2bd61c0d21cbed605fad2348
kekebeen <bbabic081@gmail.com>
2021-01-22 19:22:32 +0100

    chore(styles): add base css architecture

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { App } from 'modules/root/App';
+
+import { reportWebVitals } from './reportWebVitals';
+import 'assets/styles/app.css';
 
 ReactDOM.render(
   <React.StrictMode>
