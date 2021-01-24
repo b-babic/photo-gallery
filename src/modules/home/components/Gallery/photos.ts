2ecc303489136d26b7ee9ef976d969b62518fa91
kekebeen <bbabic081@gmail.com>
2021-01-24 16:42:17 +0100

    refactor(images): add new placeholder image sources

--- src/modules/home/components/Gallery/photos.ts
+++ src/modules/home/components/Gallery/photos.ts
@@ -6,74 +6,50 @@ export interface Photo {
 export const photos: Photo[] = [
   {
     title: 'Image 1',
-    // src: 'https://via.placeholder.com/150x250',
-    src:
-      'https://images.unsplash.com/photo-1610694955012-9d53be7baeb7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=250&fit=max',
+    src: 'https://via.placeholder.com/150x250',
   },
   {
     title: 'Image 2',
-    // src: 'https://via.placeholder.com/150x250',
-    src:
-      'https://images.unsplash.com/photo-1609242021825-19abcea33bd4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=250&fit=max',
+    src: 'https://via.placeholder.com/150x250',
   },
   {
     title: 'Image 3',
-    // src: 'https://via.placeholder.com/250x150',
-    src:
-      'https://images.unsplash.com/photo-1609535766154-e47e5aa87789?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=250&fit=max',
+    src: 'https://via.placeholder.com/250x150',
   },
   {
     title: 'Image 4',
-    // src: 'https://via.placeholder.com/150x250',
-    src:
-      'https://images.unsplash.com/photo-1609752551725-60241efa2aa8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=640&fit=max',
+    src: 'https://via.placeholder.com/150x250',
   },
   {
     title: 'Image 5',
-    // src: 'https://via.placeholder.com/250x150',
-    src:
-      'https://images.unsplash.com/photo-1610831655344-d2ab131b614f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=150&fit=max',
+    src: 'https://via.placeholder.com/250x150',
   },
   {
     title: 'Image 6',
-    // src: 'https://via.placeholder.com/150x250',
-    src:
-      'https://images.unsplash.com/photo-1610980205583-c73e590c19fd?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=720&fit=max',
+    src: 'https://via.placeholder.com/150x250',
   },
   {
     title: 'Image 7',
-    // src: 'https://via.placeholder.com/250x150',
-    src:
-      'https://images.unsplash.com/photo-1609700731241-d4b42fa1f483?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=240&fit=max',
+    src: 'https://via.placeholder.com/250x150',
   },
   {
     title: 'Image 8',
-    // src: 'https://via.placeholder.com/350x300',
-    src:
-      'https://images.unsplash.com/photo-1609519883504-0c618498b325?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=192&fit=max',
+    src: 'https://via.placeholder.com/350x300',
   },
   {
     title: 'Image 9',
-    // src: 'https://via.placeholder.com/150x250',
-    src:
-      'https://images.unsplash.com/photo-1610460894068-e664273a2195?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=300&fit=max',
+    src: 'https://via.placeholder.com/150x250',
   },
   {
     title: 'Image 10',
-    // src: 'https://via.placeholder.com/800x600',
-    src:
-      'https://images.unsplash.com/photo-1610121866634-3b7fa10ffe8c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=640&fit=max',
+    src: 'https://via.placeholder.com/800x600',
   },
   {
     title: 'Image 11',
-    // src: 'https://via.placeholder.com/250x150',
-    src:
-      'https://images.unsplash.com/photo-1609333902660-0123cce702dd?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=720&fit=max',
+    src: 'https://via.placeholder.com/250x150',
   },
   {
     title: 'Image 12',
-    // src: 'https://via.placeholder.com/150x250',
-    src:
-      'https://images.unsplash.com/photo-1609095418243-2db85af07a25?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=640&fit=max',
+    src: 'https://via.placeholder.com/150x250',
   },
 ];
