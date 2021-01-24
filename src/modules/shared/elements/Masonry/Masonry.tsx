51a4ee75f5ec3e10011bbcf780df617cee3aaba0
kekebeen <bbabic081@gmail.com>
2021-01-24 16:43:57 +0100

    feat(masonry): add masonry like layout component

--- src/modules/shared/elements/Masonry/Masonry.tsx
+++ src/modules/shared/elements/Masonry/Masonry.tsx
@@ -1,18 +1,35 @@
 import * as React from 'react';
 
+import { removeKeys } from './utils/remove-keys';
 import { uid } from './utils/uid';
+
 import styles from './Masonry.module.css';
 
-type Props = {
+interface Props {
   columnWidth?: number;
   container?: React.ReactElement<any>;
   item?: React.ReactElement<any>;
   rowGap?: number;
+  innerRef?: any;
   ref?: React.RefObject<React.ComponentType<Props> & { resize: () => void }>;
-};
+}
 
-// eslint-disable-next-line import/no-default-export
-export default class MasonryLayout extends React.Component<Props> {
+// since there is no good way to set cs --var in react yet.
+const DELTA: number = 0.125;
+const INITIAL_DELAY: number = 1.3;
+
+const blackListProps = ['rowGap', 'columnWidth', 'uniqueid'];
+const removeBlackListed = removeKeys(blackListProps);
+
+class Masonry extends React.Component<Props> {
+  // eslint-disable-next-line react/static-property-placement
+  static defaultProps = {
+    columnWidth: 260,
+    container: React.createElement('div'),
+    item: React.createElement('article'),
+    rowGap: 10,
+  };
+
   gridId: string | null = null;
 
   reposition: boolean = false;
@@ -20,20 +37,6 @@ export default class MasonryLayout extends React.Component<Props> {
   elements: HTMLElement[] = [];
 
   grid: null | HTMLElement = null;
-
-  // eslint-disable-next-line
-  static defaultProps = {
-    columnWidth: 250,
-    container: React.createElement('div'),
-    item: React.createElement('article'),
-    rowGap: 10,
-  };
-
-  constructor(props: Props) {
-    super(props);
-
-    this.gridId = uid();
-  }
 
   componentDidMount() {
     this.setValues();
@@ -65,7 +68,6 @@ export default class MasonryLayout extends React.Component<Props> {
         const itemHeight: number = firstElement.getBoundingClientRect().height;
         const rowSpan: number = Math.ceil((itemHeight + rowGap) / rowGap);
 
-        itemRef.classList.add(styles.item);
         itemRef.style.gridRowEnd = `span ${rowSpan}`;
       }
     });
@@ -104,7 +106,13 @@ export default class MasonryLayout extends React.Component<Props> {
   };
 
   render() {
-    const { children, item = React.createElement('div'), container = React.createElement('div') } = this.props;
+    const {
+      children,
+      item = React.createElement('article'),
+      container = React.createElement('div'),
+      innerRef,
+      ...compProps
+    } = this.props;
 
     const hasChildren: boolean = React.Children.count(children) > 0;
 
@@ -118,23 +126,32 @@ export default class MasonryLayout extends React.Component<Props> {
 
     const refGrid = (node: HTMLElement): void => {
       this.grid = node;
+
+      if (innerRef && innerRef.current) {
+        innerRef.current = node;
+        innerRef.current.resize = this.setValues;
+      }
     };
 
-    const renderChildren = React.Children.map(children, (child: React.ReactNode) => {
-      const itemProps = {
+    const renderChildren = React.Children.map(children, (child: React.ReactNode, index: number) => {
+      const itemProps = removeBlackListed({
         key: uid(),
         ref: refItem,
-      };
+        className: styles.item,
+      });
 
       if (item) {
-        return React.createElement(item.type, itemProps, child);
+        return React.createElement(
+          item.type,
+          { style: { ...item.props, animationDelay: `${index * DELTA + INITIAL_DELAY}s` }, ...itemProps },
+          child
+        );
       }
 
       return null;
     });
 
     const containerProps = {
-      ...container.props,
       className: styles.container,
       ref: refGrid,
     };
@@ -144,8 +161,17 @@ export default class MasonryLayout extends React.Component<Props> {
         {React.createElement('div', {
           dangerouslySetInnerHTML: { __html: this.createGridStyle() },
         })}
-        {React.createElement(container.type, containerProps, renderChildren)}
+        {React.createElement(
+          container.type,
+          { style: { ...container.props, ...compProps }, ...containerProps },
+          renderChildren
+        )}
       </>
     );
   }
 }
+
+const ExportableLayout = React.forwardRef((props, ref) => <Masonry innerRef={ref} {...props} />);
+
+// eslint-disable-next-line import/no-default-export
+export default (ExportableLayout as unknown) as React.ComponentType<Props> & { resize: () => void };
