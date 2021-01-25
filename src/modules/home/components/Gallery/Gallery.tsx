c6ac3aa88bec5ce5d59d8995a574514f625ac6bf
kekebeen <bbabic081@gmail.com>
2021-01-25 01:46:40 +0100

    refactor(spacing): make spacing more consistent accross the components

--- src/modules/home/components/Gallery/Gallery.tsx
+++ src/modules/home/components/Gallery/Gallery.tsx
@@ -6,31 +6,27 @@ import { photos, Photo } from './data/photos';
 import { useRandomDetails } from './data/use-random-details';
 import { GalleryItem } from './components/GalleryItem';
 
-import styles from './Gallery.module.css';
-
 interface Props {}
 
 export const Gallery: FunctionComponent<Props> = (): ReactElement => {
   const { hashtag, time } = useRandomDetails();
 
   return (
-    <>
-      <section className={styles.gallery}>
-        <Masonry>
-          {photos.map((photo: Photo) => {
-            return (
-              <GalleryItem
-                key={photo.title}
-                src={photo.src}
-                title={photo.title}
-                description={photo.description}
-                hashtag={hashtag}
-                time={time}
-              />
-            );
-          })}
-        </Masonry>
-      </section>
-    </>
+    <section className="section">
+      <Masonry>
+        {photos.map((photo: Photo) => {
+          return (
+            <GalleryItem
+              key={photo.title}
+              src={photo.src}
+              title={photo.title}
+              description={photo.description}
+              hashtag={hashtag}
+              time={time}
+            />
+          );
+        })}
+      </Masonry>
+    </section>
   );
 };
