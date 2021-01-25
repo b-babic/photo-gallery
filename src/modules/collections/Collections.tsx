060d99e1110d7019fef555aa897c50d3ef77db38
kekebeen <bbabic081@gmail.com>
2021-01-25 02:55:44 +0100

    feat(collections seo): add collections page seo

--- src/modules/collections/Collections.tsx
+++ src/modules/collections/Collections.tsx
@@ -1,13 +1,22 @@
 import React, { FunctionComponent, ReactElement } from 'react';
+import { useHead } from 'hoofd';
 
 import { Container } from 'modules/shared/primitives/Container/Container';
 
 import { Hero } from './components/Hero';
 import { Gallery } from './components/Gallery';
+import { useCollectionsPageSeo } from './data/use-collections-page-seo';
 
 interface Props {}
 
 export const CollectionsPage: FunctionComponent<Props> = (): ReactElement => {
+  // Set basic SEO for collections page
+  const { title, metas } = useCollectionsPageSeo();
+  useHead({
+    title,
+    metas,
+  });
+
   return (
     <main role="main">
       <Container>
