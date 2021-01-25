9d39b21eee11d82a582002fc6daf8effce840145
kekebeen <bbabic081@gmail.com>
2021-01-25 01:10:33 +0100

    feat(data): add description to the photos array

---
+++ src/modules/home/components/Gallery/data/photos.ts
@@ -0,0 +1,80 @@
+export interface Photo {
+  title: string;
+  src: string;
+  description: string;
+}
+
+export const photos: Photo[] = [
+  {
+    title: 'Image 1',
+    src: 'https://via.placeholder.com/1920x1080',
+    description:
+      'One advanced diverted domestic sex repeated bringing you old. Possible procured her trifling laughter thoughts property she met way. Companions shy had solicitude favourable own. Which could saw guest man now heard but. Lasted my coming uneasy marked so should. Gravity letters it amongst herself dearest an windows by. Wooded ladies she basket season age her uneasy saw. Discourse unwilling am no described dejection incommode no listening of. Before nature his parish boy.',
+  },
+  {
+    title: 'Image 2',
+    src: 'https://via.placeholder.com/263x438',
+    description:
+      'Folly words widow one downs few age every seven. If miss part by fact he park just shew. Discovered had get considered projection who favourable. Necessary up knowledge it tolerably. Unwilling departure education is be dashwoods or an. Use off agreeable law unwilling sir deficient curiosity instantly. Easy mind life fact with see has bore ten. Parish any chatty can elinor direct for former. Up as meant widow equal an share least. ',
+  },
+  {
+    title: 'Image 3',
+    src: 'https://via.placeholder.com/250x150',
+    description:
+      'Another journey chamber way yet females man. Way extensive and dejection get delivered deficient sincerity gentleman age. Too end instrument possession contrasted motionless. Calling offence six joy feeling. Coming merits and was talent enough far. Sir joy northward sportsmen education. Discovery incommode earnestly no he commanded if. Put still any about manor heard.',
+  },
+  {
+    title: 'Image 4',
+    src: 'https://via.placeholder.com/263x438',
+    description:
+      'Village did removed enjoyed explain nor ham saw calling talking. Securing as informed declared or margaret. Joy horrible moreover man feelings own shy. Request norland neither mistake for yet. Between the for morning assured country believe. On even feet time have an no at. Relation so in confined smallest children unpacked delicate. Why sir end believe uncivil respect. Always get adieus nature day course for common. My little garret repair to desire he esteem. ',
+  },
+  {
+    title: 'Image 5',
+    src: 'https://via.placeholder.com/250x150',
+    description:
+      'In it except to so temper mutual tastes mother. Interested cultivated its continuing now yet are. Out interested acceptance our partiality affronting unpleasant why add. Esteem garden men yet shy course. Consulted up my tolerably sometimes perpetual oh. Expression acceptance imprudence particular had eat unsatiable. ',
+  },
+  {
+    title: 'Image 6',
+    src: 'https://via.placeholder.com/263x438',
+    description:
+      'Had denoting properly jointure you occasion directly raillery. In said to of poor full be post face snug. Introduced imprudence see say unpleasing devonshire acceptance son. Exeter longer wisdom gay nor design age. Am weather to entered norland no in showing service. Nor repeated speaking shy appetite. Excited it hastily an pasture it observe. Snug hand how dare here too.',
+  },
+  {
+    title: 'Image 7',
+    src: 'https://via.placeholder.com/250x150',
+    description:
+      'Improve ashamed married expense bed her comfort pursuit mrs. Four time took ye your as fail lady. Up greatest am exertion or marianne. Shy occasional terminated insensible and inhabiting gay. So know do fond to half on. Now who promise was justice new winding. In finished on he speaking suitable advanced if. Boy happiness sportsmen say prevailed offending concealed nor was provision. Provided so as doubtful on striking required. Waiting we to compass assured. ',
+  },
+  {
+    title: 'Image 8',
+    src: 'https://via.placeholder.com/350x300',
+    description:
+      'You disposal strongly quitting his endeavor two settling him. Manners ham him hearted hundred expense. Get open game him what hour more part. Adapted as smiling of females oh me journey exposed concern. Met come add cold calm rose mile what. Tiled manor court at built by place fanny. Discretion at be an so decisively especially. Exeter itself object matter if on mr in. ',
+  },
+  {
+    title: 'Image 9',
+    src: 'https://via.placeholder.com/263x438',
+    description:
+      'Effect if in up no depend seemed. Ecstatic elegance gay but disposed. We me rent been part what. An concluded sportsman offending so provision mr education. Bed uncommonly his discovered for estimating far. Equally he minutes my hastily. Up hung mr we give rest half. Painful so he an comfort is manners. ',
+  },
+  {
+    title: 'Image 10',
+    src: 'https://via.placeholder.com/800x600',
+    description:
+      'Article nor prepare chicken you him now. Shy merits say advice ten before lovers innate add. She cordially behaviour can attempted estimable. Trees delay fancy noise manor do as an small. Felicity now law securing breeding likewise extended and. Roused either who favour why ham. ',
+  },
+  {
+    title: 'Image 11',
+    src: 'https://via.placeholder.com/250x150',
+    description:
+      'Terug eenig der leven ter als nam. Al ad te bezit komst ficus op. Is ploeg of groen ze wijze japan er. Boringen langzaam ik kolonien te strooien ze. Ons ander enkel bak roode dit sinds meest het. Dit had geslaagd generaal een afstands. Maleiers bevatten deeltjes behoefte in mogelijk en. ',
+  },
+  {
+    title: 'Image 12',
+    src: 'https://via.placeholder.com/263x438',
+    description:
+      'Hoogere dus nam tijdens aandeel talrijk. Begrenzing uiteenvalt nu ontsnappen initiatief of werkwijzen. Der hooge aldus hitte sap tegen als. Ringen is zoodat daarin marmer af bezoek ze soegei. Op kaal geld acre om af west. In hout om liep doet is af. In tweeden malakka ik moesten valorem af al. Goudmijnen voorschijn verwijderd europeesch uit sap verzekeren. Gewijzigd krachtige dat alluviale gas waaronder verklaart evenwicht.',
+  },
+];
